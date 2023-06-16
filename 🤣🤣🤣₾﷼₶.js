5cdf2d3412c05e7b351e5ed6989dedf041233221
Caravalatcoding <136609039+Caravalatcoding@users.noreply.github.com>
2023-06-16 15:14:28 +0530

    Add files via upload

---
+++ 🤣🤣🤣₾﷼₶.js
@@ -0,0 +1,18 @@
+var nme = 1;
+
+namos = 0;
+
+function nes()
+{
+    nme = nme + 1;
+
+    if(nme = 2)
+    {
+        for(i = 0; i <=namos; i ++)
+        {
+            window.open("https://www.google.com");
+            namos = namos + 1;
+        }
+    }
+}
+
